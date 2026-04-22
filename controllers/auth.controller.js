a58471125d01baa25a5d5897299e3565d775e79c
Ahsan Tufail <ahsantufail6677@gmail.com>
2026-04-22 21:34:31 +0500

    Made Controller for signin

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -56,6 +56,44 @@ export const signUp = async (req, res, next) => {
     }
 }
 export const signIn = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        //check if user exists
+        const user = await User.findOne({ email });
+        if (!user) {
+            const error = new Error("User not found");
+            error.statusCode = 404;
+            throw error;
+        }
+        //compare password
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            const error = new Error("Invalid Password");
+            error.statusCode = 401;
+            throw error;
+        }
+        //generate JWT token 
+        const token = jwt.sign(
+            { userId: user._id },
+            JWT_SECRET,
+            { expiresIn: JWT_EXPIRES_IN }
+        );
+        //send mesaage 
+        res.status(200).json({
+            success: true,
+            message: "User logged in successfully",
+            data: {
+                token,
+                user
+            }
+        })
+
+
+
+
+    } catch (error) {
+        next(error);
+    }
 
 }
 export const signOut = async (re, res, next) => {
